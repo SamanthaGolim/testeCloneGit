fb570b7e3180cab5f920cd7f01a06436fd1cd212
filipe <filipe13freitas@gmail.com>
2020-02-14 08:19:54 -0300

    idade

--- teste.c
+++ teste.c
@@ -3,6 +3,14 @@
 
 int main(){
 
+    int idade, result;
+
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
+
+    result = idade-1;
+
+    printf("Resultado: %i\n", result);
 
     return 0;
 }
